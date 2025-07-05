4c46772ca96041d00b1806c69b316d733d10e163
Sharavana Kumar <158765639+Sharavanakumar-Ramalingam@users.noreply.github.com>
2025-07-05 22:33:26 +0530

    Update Triangle2.java

--- Wipro Java Programs/Triangle2.java
+++ Wipro Java Programs/Triangle2.java
@@ -1,14 +1,26 @@
-import java.util.Scanner;
-public class Triangle2{
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++)
-                System.out.print("  ");
-            for (int j = 1; j <= i; j++)
-                System.out.print("* ");
-            System.out.println();
+public static void main(String[] args)  {
+      Scanner sc=new Scanner(System.in);
+    System.err.println("Enter the number of rows for the pattern:");
+        int n = sc.nextInt();
+     int i, k, flag = 1;
+        for (i = 1, k = 0; i <= 2 * n - 1; i++) {
+            if (i < n - k)
+                System.out.print(" ");
+            else {
+                if (flag == 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+                flag = 1 - flag;
+            }
+            if (i == n + k) {
+                k++;
+                System.out.println();
+                if (i == 2 * n - 1)
+                    break;
+                i = 0;
+                flag = 1;
+            }
         }
     }
 }
